a8086891dc8afbc929190969db83286031fb31cc
Sebas <sebas.cay@gmail.com>
2022-03-01 20:05:06 -0500

    Crear Estilos Basicos Con Bootstrap E Imprimpir Canciones Traversin

--- servicios.js
+++ servicios.js
@@ -1,6 +1,6 @@
 
 const URI = "https://api.spotify.com/v1/artists/5NGO30tJxFlKixkPSgXcFE/top-tracks?market=US"
-const TOKEN = "Bearer BQDgEMWfOOc-Yg9bkFuFTb1Uv-rJAZLfI7IfX-CSbBRpLr9-eYlVBnmWh3VnOP1VT7HQUVYwOn1sYBXwU4eAHXudnrJgdmNfwjCDw-GH0aPqFQ53oyCIgob9JbDtC1BSAYcrBAwAByLkUFcX"
+const TOKEN = "Bearer BQDyoS99-93q6wi_dCpHejnV_96WAEseVO7v36JZmDvBJBv7FCRVPqTxtWovCgprP40cHCrVsHQ-EhbabaC1tGnwdX6hfZkLdzsWkxFLDJDfuu0m-SYmx-0Q9uIQJlU99Axxz5OZEVBBR07C"
 
 let parametrosPeticion = {
     method:"GET",
@@ -9,11 +9,27 @@ let parametrosPeticion = {
     }  
 }
 
+const songTemplates = document.querySelector("[data-song-template]")
+const container = document.querySelector("[data-container]")
 fetch(URI,parametrosPeticion)
-.then(respuesta=>respuesta.json())
-.then(respuestaJSON=>{
-    respuestaJSON.tracks.forEach(track => {
-        console.log(track.name)
+.then(res=>res.json())
+.then(data =>{
+    data.tracks.forEach(track => {
+        const cont= songTemplates.content.cloneNode(true).children[0]
+        const img = cont.querySelector("img")
+        const songName = cont.querySelector("p")
+        img.src = track.album.images[0].url
+        img.height = track.album.images[2].height
+        img.width = track.album.images[2].width
+        songName.textContent = track.name
+        container.appendChild(cont)
+        console.log(track)
     });
 })
 .catch(respuestaERROR=>console.log(respuestaERROR))
+
+
+
+
+
+
