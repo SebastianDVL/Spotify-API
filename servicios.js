bc6b1d796e473d1d8c25f7670d720bced93b285c
401 <401@academicos.cesde.edu.co>
2022-03-03 11:29:17 -0500

    Se organiza los estilos de las canciones y se agrega audio

--- servicios.js
+++ servicios.js
@@ -1,6 +1,6 @@
 
 const URI = "https://api.spotify.com/v1/artists/5NGO30tJxFlKixkPSgXcFE/top-tracks?market=US"
-const TOKEN = "Bearer BQDyoS99-93q6wi_dCpHejnV_96WAEseVO7v36JZmDvBJBv7FCRVPqTxtWovCgprP40cHCrVsHQ-EhbabaC1tGnwdX6hfZkLdzsWkxFLDJDfuu0m-SYmx-0Q9uIQJlU99Axxz5OZEVBBR07C"
+const TOKEN = "Bearer BQBcI5Q8a3e4135K9A2oytlG3DVenxll6BUyoEMAGu4VzN4e48G_Bc2EfSnVSNJgB8PN0cM5-0rGft-ZW3yFUZnWAOEtxLaeWJfBWZ3ULXg42UQUXNFobPkxdze8_CxC8Fhx1mBExidbp9zx"
 
 let parametrosPeticion = {
     method:"GET",
@@ -13,22 +13,35 @@ const songTemplates = document.querySelector("[data-song-template]")
 const container = document.querySelector("[data-container]")
 fetch(URI,parametrosPeticion)
 .then(res=>res.json())
-.then(data =>{
-    data.tracks.forEach(track => {
-        const cont= songTemplates.content.cloneNode(true).children[0]
-        const img = cont.querySelector("img")
-        const songName = cont.querySelector("p")
+.then(data=>{
+    data.tracks.forEach((track,index) => {
+        let cont= songTemplates.content.cloneNode(true).children[0]
+        let img = cont.querySelector("img")
+        let songName = cont.querySelector(".name")
+        let album = cont.querySelector("p")
+        let year = cont.querySelector("small")
+        let i = cont.querySelector("h6")
+        let duration  = cont.querySelector(".duration")
+        let audio = cont.querySelector("audio")
         img.src = track.album.images[0].url
         img.height = track.album.images[2].height
         img.width = track.album.images[2].width
         songName.textContent = track.name
+        album.textContent = track.album.name
+        audio.src = track.preview_url
+        year.textContent =track.album.release_date.substring(0,4)
+        i.textContent = index + 1
+        duration.textContent = msToMinutes(track.duration_ms)
         container.appendChild(cont)
-        console.log(track)
-    });
+    })
 })
 .catch(respuestaERROR=>console.log(respuestaERROR))
 
-
+function msToMinutes(ms) {
+    let minutos = Math.floor(ms / 60000);
+    let segundos = ((ms % 60000) / 1000).toFixed(0);
+    return minutos + ":" + (segundos < 10 ? '0' : '') + segundos;
+  }
 
 
 
