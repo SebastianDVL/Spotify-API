4e18bfb00adc4f1c3eed15f35f8213cb3061945a
Sebas <sebas.cay@gmail.com>
2022-03-03 21:03:47 -0500

    start customizin audio player

--- servicios.js
+++ servicios.js
@@ -1,6 +1,6 @@
 
 const URI = "https://api.spotify.com/v1/artists/5NGO30tJxFlKixkPSgXcFE/top-tracks?market=US"
-const TOKEN = "Bearer BQBcI5Q8a3e4135K9A2oytlG3DVenxll6BUyoEMAGu4VzN4e48G_Bc2EfSnVSNJgB8PN0cM5-0rGft-ZW3yFUZnWAOEtxLaeWJfBWZ3ULXg42UQUXNFobPkxdze8_CxC8Fhx1mBExidbp9zx"
+const TOKEN = "Bearer BQC19z6dVZOaYmXzw2ZqHXlmGwUZ-NYyV84rOuWudcDZA1cRwjCGhOifNE09md7fEilMNkm_bQX4zmBE5k9GJRd3EzMAWdlU_KCAEniNwBoG5up0U7O19ZZ9CgeRxq6otpJd-Zoxvs5nXtp8"
 
 let parametrosPeticion = {
     method:"GET",
@@ -14,6 +14,9 @@ const container = document.querySelector("[data-container]")
 fetch(URI,parametrosPeticion)
 .then(res=>res.json())
 .then(data=>{
+    let audio = document.querySelector("audio")
+    audio.src = data.tracks[0].preview_url
+
     data.tracks.forEach((track,index) => {
         let cont= songTemplates.content.cloneNode(true).children[0]
         let img = cont.querySelector("img")
@@ -22,13 +25,13 @@ fetch(URI,parametrosPeticion)
         let year = cont.querySelector("small")
         let i = cont.querySelector("h6")
         let duration  = cont.querySelector(".duration")
-        let audio = cont.querySelector("audio")
+        
         img.src = track.album.images[0].url
         img.height = track.album.images[2].height
         img.width = track.album.images[2].width
         songName.textContent = track.name
         album.textContent = track.album.name
-        audio.src = track.preview_url
+        
         year.textContent =track.album.release_date.substring(0,4)
         i.textContent = index + 1
         duration.textContent = msToMinutes(track.duration_ms)
@@ -41,7 +44,13 @@ function msToMinutes(ms) {
     let minutos = Math.floor(ms / 60000);
     let segundos = ((ms % 60000) / 1000).toFixed(0);
     return minutos + ":" + (segundos < 10 ? '0' : '') + segundos;
-  }
+ }
+
+let playButton = document.querySelector('.fa-play')
+let audio = document.querySelector("audio")
+ playButton.addEventListener('click',()=>{
+     audio.play()
+ })
 
 
 
